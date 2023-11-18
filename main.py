0ff5e7ffea3e24c48f23a6031e8ec902f36604a4
firexrwt <stepanveremeev@gmail.com>
2023-11-19 00:35:42 +0100

    Regular Project Update

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from properties import *
 import moderngl as mgl
 import pygame as pg
 import sys
+from shader_program import ShaderProgram
 
 
 class VoxelGame:
     def __init__(self):
+        self.shader_program = None
         pg.init()
         pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
         pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
@@ -18,17 +20,41 @@ class VoxelGame:
         self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE | mgl.BLEND)
         self.ctx.gc_mode = "auto"
 
+        self.clock = pg.time.Clock()
+        self.delta_time = 0
+        self.time = 0
+
+        self.is_running = True
+        self.on_init()
+
+    def on_init(self):
+        self.shader_program = ShaderProgram(self)
+
     def update(self):
-        pass
+        self.shader_program.update()
+
+        self.delta_time = self.clock.tick()
+        self.time = pg.time.get_ticks() / 1000
+        pg.display.set_caption(f"{window_title} | FPS: {self.clock.get_fps()}")
 
     def render(self):
-        pass
+        self.ctx.clear()
+        pg.display.flip()
 
     def handle_events(self):
-        pass
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                self.is_running = False
+                pg.quit()
+                sys.exit()
 
     def run(self):
-        pass
+        while self.is_running:
+            self.handle_events()
+            self.update()
+            self.render()
+        pg.quit()
+        sys.exit()
 
 
 if __name__ == "__main__":
---
+++ shader_program.py
@@ -0,0 +1,23 @@
+from properties import *
+
+
+class ShaderProgram:
+    def __init__(self, app):
+        self.app = app
+        self.ctx = app.ctx
+        self.quad = self.get_program("quad")
+        self.set_uniforms_on_init()
+
+    def set_uniforms_on_init(self):
+        pass
+
+    def update(self):
+        pass
+
+    def get_program(self, shader_name):
+        with open(f"shaders/{shader_name}.vert") as f:
+            vertex_shader = f.read()
+        with open(f"shaders/{shader_name}.frag") as f:
+            fragment_shader = f.read()
+        program = self.ctx.program(vertex_shader, fragment_shader)
+        return program
